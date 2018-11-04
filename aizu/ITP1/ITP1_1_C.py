469a0941e20d3c364e878d908bb99f02d9a17498
takashi yonezawa <ynzwtks@gmail.com>
2018-11-04 20:34:02 +0900

    Revert "test commit"
    This reverts commit 2365c05b960665745066bc0b3ad366f1dbc43d19.

--- aizu/ITP1/ITP1_1_C.py
+++
@@ -1,7 +0,0 @@
-
-try:
-     (a,b)  =  map(int,input().split())
-except:
-    exit
-
-print (a*b,(a+b)*2)
--- aizu/ITP1/ITP1_2_C.py
+++
@@ -1,8 +0,0 @@
-try:
-    (a,b,c)  =  map(int,input().split())
-except:
-    exit
-
-n = [a,b,c]
-n.sort()
-print ('{0[0]} {0[1]} {0[2]}'.format(n))
